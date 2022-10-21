735bca4cc6126a36d474a05760395ee25a310f32
Adil <adilsattar674@gmail.com>
2022-10-21 18:13:35 +0500

    Initial commit

---
+++ app/blinking/src/main/java/com/lib/blinking/AdilBlinking.java
@@ -0,0 +1,10 @@
+package com.lib.blinking;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class AdilBlinking {
+    public static void showBlinking(Context context){
+        Toast.makeText(context, "Thanks for Blinking", Toast.LENGTH_SHORT).show();
+    }
+}
